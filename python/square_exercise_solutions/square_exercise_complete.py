f8f9b94d2763e6d1eb84e2dae36d6e4d18fb948e
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 07:47:13 -0800

    question_5

--- python/square_exercise_solutions/square_exercise_complete.py
+++ python/square_exercise_solutions/square_exercise_complete.py
@@ -5,16 +5,13 @@ from psychopy import visual,event,core # import the bits of PsychoPy we'll need 
 win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
 square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100]) #create a Rectangle type object with certain parameters
 
-colors = ["blue","red"]*3
-
-for cur_color in colors:
-    square.setFillColor(cur_color)
-    square.draw()
-    win.flip()
-    core.wait(1)
-    win.flip()
-    core.wait(.05)
-
+square.draw()
+win.flip()
+core.wait(1)
+square.ori = 45
+square.draw()
+win.flip()
+core.wait(1)
 
 win.close() #close the window -- don't need this if you're running this as a separate file
 core.quit() #quit out of the program -- don't need this if you're running this as a separate file
