9d49883c83e95b47e6239d0a78e5e583c1bd4cf8
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 07:35:10 -0800

    remove frame rate check

--- python/square_exercise/square_exercise.py
+++ python/square_exercise/square_exercise.py
@@ -2,7 +2,7 @@ import time
 import sys
 from psychopy import visual,event,core # import the bits of PsychoPy we'll need for this exercise
 
-win = visual.Window([400,400],color="black", units='pix') #open a window
+win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
 square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100]) #create a Rectangle type object with certain parameters
 square.draw() #draw the square to the screen buffer
 win.flip() #make the buffer visible, i.e., show what's been drawn to it
