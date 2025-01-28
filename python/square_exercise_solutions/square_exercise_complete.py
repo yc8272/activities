d6f5c72b17dd2e9ca74b9b0a3db5106edad499b3
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 07:51:51 -0800

    question_6

--- python/square_exercise_solutions/square_exercise_complete.py
+++ python/square_exercise_solutions/square_exercise_complete.py
@@ -5,13 +5,13 @@ from psychopy import visual,event,core # import the bits of PsychoPy we'll need 
 win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
 square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100]) #create a Rectangle type object with certain parameters
 
-square.draw()
-win.flip()
-core.wait(1)
-square.ori = 45
-square.draw()
-win.flip()
-core.wait(1)
+#spinning!!!!
+while True:
+    square.draw()
+    win.flip()
+    square.ori +=6 #assuming 60 Hz refresh rate, so 6*60 ~360 degrees per second
+    if event.getKeys('q'):
+        break
 
 win.close() #close the window -- don't need this if you're running this as a separate file
 core.quit() #quit out of the program -- don't need this if you're running this as a separate file
