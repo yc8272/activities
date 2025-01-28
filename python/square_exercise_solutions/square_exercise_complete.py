be7264da6e5d71e35fbe9a5846c7e35120b85848
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 07:37:17 -0800

    question_1

--- python/square_exercise_solutions/square_exercise_complete.py
+++ python/square_exercise_solutions/square_exercise_complete.py
@@ -3,7 +3,7 @@ import sys
 from psychopy import visual,event,core # import the bits of PsychoPy we'll need for this exercise
 
 win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
-square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100]) #create a Rectangle type object with certain parameters
+square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100]) #create a Rectangle type object with certain parameters
 square.draw() #draw the square to the screen buffer
 win.flip() #make the buffer visible, i.e., show what's been drawn to it
 core.wait(.5) #pause for half a second (i.e., 500 ms)
