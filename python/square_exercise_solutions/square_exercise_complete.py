ab7faa9232f0e0759cc956a8c964d1b910657d26
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 08:10:23 -0800

    question_10

--- python/square_exercise_solutions/square_exercise_complete.py
+++ python/square_exercise_solutions/square_exercise_complete.py
@@ -3,15 +3,21 @@ import sys
 from psychopy import visual,event,core # import the bits of PsychoPy we'll need for this exercise
 
 win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #open a window
-square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100]) #create a Rectangle type object with certain parameters
+left_square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100],pos = (-100,0)) #create a Rectangle type object with certain parameters
+right_square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100],pos = (100,0)) #create a Rectangle type object with certain parameters
 
+spinning = True
 while True:
-    if event.getKeys('left'):
-        square.size += [10,0]
-    elif event.getKeys('right'):
-        square.size += [-10,0]
-    square.draw()
-    win.flip()
+    if spinning:
+        left_square.ori += 6
+        left_square.draw()
+        right_square.ori += -6
+        right_square.draw()
+        win.flip()
+    if event.getKeys('s'):
+        spinning = False
+    if event.getKeys('r'):
+        spinning = True
     if event.getKeys('q'):
         break
 
