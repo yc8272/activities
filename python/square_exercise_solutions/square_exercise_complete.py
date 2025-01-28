e6c6e0e2dc493c82583bd7078743dc9311020d78
mzettersten <mzettersten@users.noreply.github.com>
2025-01-28 07:39:27 -0800

    question_2

--- python/square_exercise_solutions/square_exercise_complete.py
+++ python/square_exercise_solutions/square_exercise_complete.py
@@ -6,6 +6,6 @@ win = visual.Window([400,400],color="black", units='pix',checkTiming=False) #ope
 square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100]) #create a Rectangle type object with certain parameters
 square.draw() #draw the square to the screen buffer
 win.flip() #make the buffer visible, i.e., show what's been drawn to it
-core.wait(.5) #pause for half a second (i.e., 500 ms)
+core.wait(1.5) #pause for half a second (i.e., 500 ms)
 win.close() #close the window -- don't need this if you're running this as a separate file
 core.quit() #quit out of the program -- don't need this if you're running this as a separate file
